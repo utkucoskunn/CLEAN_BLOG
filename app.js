9b7ab091d79b63a549ec590650e24801d9ab1c8e
UTKU COŞKUN <70485246+utkucoskunn@users.noreply.github.com>
2022-04-08 21:39:09 +0300

    TEMPLATE ENGINE, MIDDLEWARES, ROUTES added...

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
 const express = require('express');
+const ejs=require('ejs');
+const path=require('path');
 
 const app = express();
 
+//TEMPLATE ENGINE
+app.set("view engine","ejs");
+
+//MIDDLEWARES
+app.use(express.static('public'));
+
+//ROUTES
 app.get('/', (req, res) => {
-    const blog = {id: 1, title: "Blog title", description: "Blog description"};
-    res.send(blog);
+    res.render('index')
 })
+
+app.get('/add_post', (req, res) => {
+    res.render('add_post')
+})
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
+app.get('/post', (req, res) => {
+    res.render('post')
+})
+
+
+
 
 const port = 3000;
 app.listen(port, () => {
